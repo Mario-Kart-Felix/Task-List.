b7c09d5cd00a9ca4ede1550cfeb8a0789eb1a3ba
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-13 09:57:07 -0500

    Create Inertia.js

---
+++ Inertia.js
@@ -0,0 +1,28 @@
+  #component: "Marketing/Overview"
+  #props: array:22 [▼
+    "amp_url" => null
+    "api_url" => null
+    "collection" => array:3 [▶]
+    "content" => array:4 [▶]
+    "date" => Illuminate\Support\Carbon @1617827556 {#2478 ▶}
+    "edit_url" => "http://mysite.test/cp/collections/marketing/entries/f854a1cf-0dcf-404b-8418-a74662ba77e7/overview"
+    "id" => "f854a1cf-0dcf-404b-8418-a74662ba77e7"
+    "is_entry" => true
+    "last_modified" => Illuminate\Support\Carbon @1617827556 {#2477 ▶}
+    "mount" => null
+    "order" => null
+    "parent" => null
+    "permalink" => "http://mysite.test/marketing"
+    "private" => false
+    "published" => true
+    "slug" => "overview"
+    "template" => "app"
+    "title" => "Overview"
+    "updated_at" => Illuminate\Support\Carbon @1617827556 {#2523 ▶}
+    "updated_by" => array:4 [▶]
+    "uri" => "/marketing"
+    "url" => "/marketing"
+  ]
+  #rootView: "app"
+  #version: ""
+  #viewData: []
