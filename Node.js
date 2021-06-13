fdc862e064d2529dfd666499cf6222daa639c5fb
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-06-13 10:12:31 -0500

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,5 @@
+
+require() any package directly from npm
+use arrow functions, classes, template strings, and most of ES6
+await any promise instead of using callbacks (example)
+create your own embedded node.js snippets
